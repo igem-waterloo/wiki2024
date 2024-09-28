723c79232d980652c7556cc4c32b6806e9b11df7
aaren-aras <aaren.aras@gmail.com>
2024-09-27 20:42:25 -0400

    Fix nav bar for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://2024.igem.wiki/waterloo',
-  outDir: './dist',
-  publicDir: './public',
+  site: 'https://2024.igem.wiki',
+  base: '/waterloo',
+  outDir: 'public',
+  publicDir: 'static',
 });
