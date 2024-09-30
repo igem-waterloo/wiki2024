cf883990209c2963af2d3f4ba5b96f925d8061bf
aaren-aras <aaren.aras@gmail.com>
2024-09-30 00:14:13 -0400

    Add more styling to stat2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://2024.igem.wiki',
-  base: '/waterloo',
+  base: '/waterloo/',
   outDir: 'public',
   publicDir: 'assets',
 });
